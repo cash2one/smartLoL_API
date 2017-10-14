3cd2da8fc7faaf1b819ac87941d7c66513e5b972
Andres Espino Lopez <espinomlg@gmail.com>
2017-10-14 12:27:11 +0200

    preparando db

--- database/setup.py
+++ database/setup.py
@@ -1,18 +1,19 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 import json
+import os.path
 
 
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 DB_URI = "mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}"
-with open("db.config", "r") as infile:
+config_file = os.path.join(os.path.dirname(__file__), "db.config")
+with open(config_file, "r") as infile:
     CONFIG = json.load(infile)
 engine = create_engine(DB_URI.format(**CONFIG))
 Base = declarative_base()
 
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
-Base.metadata.create_all(engine)
 
 
 def init_db():
@@ -21,4 +22,4 @@ def init_db():
 
 
 if __name__ == '__main__':
-    pass
+    init_db()
